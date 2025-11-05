5428b936d94955220990b2c46fc55b427d4146d4
James McAvoy <jimcavoy@thetastream.com>
2025-11-05 13:22:50 +0000

    Fixed GCC 15 errors
    Added  CMake files to easy building and installing the exip library.
    Fixed compiler errors reported by GCC version 15.

--- src/contentIO/src/bodyDecode.c
+++ src/contentIO/src/bodyDecode.c
@@ -1230,7 +1230,7 @@ errorCode decodeNSEvent(EXIStream* strm, ContentHandler* handler, SmallIndex* no
 	errorCode tmp_err_code = EXIP_UNEXPECTED_ERROR;
 	SmallIndex ns_uriId;
 	SmallIndex pfxId;
-	boolean bool = FALSE;
+	boolean b = FALSE;
 
 	DEBUG_MSG(INFO, DEBUG_CONTENT_IO, (">NS event:\n"));
 	*nonTermID_out = GR_START_TAG_CONTENT;
@@ -1243,11 +1243,11 @@ errorCode decodeNSEvent(EXIStream* strm, ContentHandler* handler, SmallIndex* no
 	}
 
 	TRY(decodePfx(strm, ns_uriId, &pfxId));
-	TRY(decodeBoolean(strm, &bool));
+	TRY(decodeBoolean(strm, &b));
 
 	if(handler->namespaceDeclaration != NULL)  // Invoke handler method
 	{
-		TRY(handler->namespaceDeclaration(strm->schema->uriTable.uri[ns_uriId].uriStr, strm->schema->uriTable.uri[ns_uriId].pfxTable->pfxStr[pfxId], bool, app_data));
+		TRY(handler->namespaceDeclaration(strm->schema->uriTable.uri[ns_uriId].uriStr, strm->schema->uriTable.uri[ns_uriId].pfxTable->pfxStr[pfxId], b, app_data));
 	}
 	return EXIP_OK;
 }
